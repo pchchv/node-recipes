404c532abf82a3d9cdb0b3e3376034fa27015ea2
pchchv <ipchchv@gmail.com>
2022-01-18 11:53:41 +0300

    refactor(app): Add a hash of the password

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -16,6 +17,13 @@ const userSchema = new mongoose.Schema({
     },
 });
 
+// fire a function before doc saved to db
+userSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  });
+  
 const User = mongoose.model('user', userSchema);
 
 module.exports = User;
