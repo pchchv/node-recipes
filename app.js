0c3d2c4bfc70a7a544462f82dd43aac05d7e0b8a
pchchv <ipchchv@gmail.com>
2022-01-19 12:27:12 +0300

    refactor(app): Add cookies

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set('view engine', 'ejs');
@@ -23,3 +25,16 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+// cookies
+app.get('/set-cookies', (req, res) => {
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, { maxAge: 86400000, httpOnly: true }); // maxAge 24 hours
+  res.send('you got the cookies');
+});
+
+app.get('/read-cookies', (req, res) => {
+  const cookies = req.cookies;
+  console.log(cookies.newUser);
+  res.json(cookies);
+});
