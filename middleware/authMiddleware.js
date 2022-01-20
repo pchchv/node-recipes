e6bc97a4050d025c12b0ec215a4b71d1be7c8804
pchchv <ipchchv@gmail.com>
2022-01-21 00:47:50 +0300

    refactor(app): Add current user check

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -21,4 +21,25 @@ const requireAuth = (req, res, next) => {
   }
 };
 
-module.exports = { requireAuth };
+// check current user
+const checkUser = (erq, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, secret, async (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.locals.user = null;
+      } else {
+        console.log(decodedToken);
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    })
+  } else {
+    res.locals.user = null;
+    next();
+  }
+} 
+module.exports = { requireAuth, checkUser };
