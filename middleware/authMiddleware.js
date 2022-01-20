c4363e2f577941993c88348e9e0ca4862a96462d
pchchv <ipchchv@gmail.com>
2022-01-21 00:28:38 +0300

    refactor(app): Add route protection (authentication check)

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
+
+const secret = fs.readFileSync("secret.txt", "utf8");
+const requireAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  // check json web token exists & is verified
+  if (token) {
+    jwt.verify(token, secret, (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.redirect('/login');
+      } else {
+        console.log(decodedToken);
+        next();
+      }
+    });
+  } else {
+    res.redirect('/login');
+  }
+};
+
+module.exports = { requireAuth };
